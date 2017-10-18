8dd1a12b95cc0c6095aa1ef94893000e4a27b57c
imprevo <zhenek42@gmail.com>
2017-10-19 00:34:31 +0700

    remove redux

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,13 +1,11 @@
 import View from './containers/ChatContainer';
-import reducer from './reducers/chatReducer';
-import Store from './stores/ChatMessageStore';
+import ChatMessageStore from './stores/ChatMessageStore';
 import ChatSocket from './api/chatWebsocket';
 
 const socket = new ChatSocket(process.env.REACT_APP_BACKEND_HOST);
-const store = new Store(socket);
+const store = new ChatMessageStore(socket);
 
 export {
-    reducer,
     View,
     store,
 };
